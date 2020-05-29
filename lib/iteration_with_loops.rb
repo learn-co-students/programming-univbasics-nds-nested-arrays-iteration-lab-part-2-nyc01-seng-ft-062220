22435acf7dc543d00aa759c4a8beb7b733673b9e
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-05-29 15:52:30 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,14 @@
 def find_min_in_nested_arrays(src)
-  # src will be an array of arrays of integers
-  # Produce a new Array that contains the smallest number of each of the nested arrays
+  lowest_temps_array = []
+  for child_array in src do
+    lowest_temp = 1000
+    for temp in child_array do
+      if temp < lowest_temp
+        lowest_temp = temp
 
+      end
+    end
+    lowest_temps_array.push(lowest_temp)
+  end
+  return lowest_temps_array
 end
